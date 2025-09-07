f82e46d7ff7e802e69051a6ede700bb960748f5c
bhavya23V <bhavyavemula23@gmail.com>
2025-09-07 22:40:28 +0530

    Add files via upload

---
+++ tuple operations PWP Lab 5.py
@@ -0,0 +1,30 @@
+# Post Lab Exercise 1: 
+t1 = (8, 5, 6, 2, 8, 4, 7, 1, 9, 0, 8, 3, 7)
+print(t1.count(8))
+
+# Post Lab Exercise 2: 
+t2 = (2, 5, 8, 1, 4)
+print(1 in t2)
+print(3 in t2)
+
+# Post Lab Exercise 3: 
+t3 = ("I", "C", "T", " ", "D", "e", "p", "a", "r", "t", "m", "e", "n", "t")
+print(''.join(t3))
+
+# Post Lab Exercise 4: 
+t4 = (8, 5, 6, 2, 8, 4, 7, 1, 9, 0, 8, 3, 7)
+print(min(t4))
+print(max(t4))
+
+# Post Lab Exercise 5: 
+t5 = ("Deva ", "Harsha ", "is ", "from ", "ICT ", "Department")
+print(''.join(t5))
+
+# Post Lab Exercise 6: 
+t6 = (2, 5, 8, 1, 4)
+print(sorted(t6))
+
+# Post Lab Exercise 7: 
+t7 = (8, 5, 6, 2, 8, 4, 7, 1, 9, 0, 8, 3, 7)
+print(t7[0])
+print(t7[-1])
